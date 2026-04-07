77185089cdb4ad1e16135cc6a7765fa08f14ce94
Serhii <serhii@example.com>
2026-04-07 19:32:58 +0200

    Filter with form

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 app.secret_key = "anything"
 items = []
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        product_name = request.form.get('name')
+        product_name = request.form.get('name').lower()
         product_price = request.form.get('price')
         product_category = request.form.get('category')
         for item in items:
@@ -19,7 +20,20 @@ def index():
                           'price': product_price,
                           'category': product_category})
         return redirect(url_for('index'))
-    return render_template('index.html', items=items)
+
+    filter_items = None
+    categories = map(lambda item: item['category'], items)
+    choice_category = request.args.get('category', 'all')
+
+    if choice_category == 'all':
+        filter_items = items
+    elif choice_category == 'category':
+        filter_items = filter(lambda item: item['category'] == choice_category, items)
+
+    return render_template('index.html',
+                           items=filter_items,
+                           categories=categories,
+                           choice_category=choice_category)
 
 @app.route('/delete/<item_index>')
 def delete(item_index):
